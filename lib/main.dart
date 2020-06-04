f5b0a3d11d672b7144ecb6cc8d9b21fc2a4988ee
THE-JEY <jey.pley@gmail.com>
2020-06-04 12:17:40 +0200

    horizontal view

--- lib/main.dart
+++ lib/main.dart
@@ -75,111 +75,116 @@ class _HomePageState extends State<HomePage> {
       },
       child: Scaffold(
         backgroundColor: Colors.grey.shade100,
-        body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage('image/tips.png'),
-                backgroundColor: Colors.grey.shade100,
-                radius: 80.0,
-              ),
-              SizedBox(height: 40.0),
-              TextField(
-                onChanged: (value) {
-                  billAmount = double.parse(value);
-                },
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hintText: 'Enter the bill amount...',
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                children: [
-                  Text('Tip %'),
-                  Spacer(),
-                  GestureDetector(
-                    onTap: () {
-                      tipDecrement();
-                    },
-                    child: Icon(Icons.remove_circle),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: AssetImage('image/tips.png'),
+                    backgroundColor: Colors.grey.shade100,
+                    radius: 100.0,
                   ),
-                  SizedBox(width: 8.0),
-                  Text('$tipPercentage %'),
-                  SizedBox(width: 8.0),
-                  GestureDetector(
-                    onTap: () {
-                      tipIncrement();
+                  SizedBox(height: 40.0),
+                  TextField(
+                    onChanged: (value) {
+                      billAmount = double.parse(value);
                     },
-                    child: Icon(Icons.add_circle),
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: 'Enter the bill amount...',
+                    ),
                   ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                children: [
-                  Text('People'),
-                  Spacer(),
-                  GestureDetector(
-                    onTap: () {
-                      peopleDecrement();
-                    },
-                    child: Icon(Icons.remove_circle),
+                  SizedBox(height: 20.0),
+                  Row(
+                    children: [
+                      Text('Tip %'),
+                      Spacer(),
+                      GestureDetector(
+                        onTap: () {
+                          tipDecrement();
+                        },
+                        child: Icon(Icons.remove_circle),
+                      ),
+                      SizedBox(width: 8.0),
+                      Text('$tipPercentage %'),
+                      SizedBox(width: 8.0),
+                      GestureDetector(
+                        onTap: () {
+                          tipIncrement();
+                        },
+                        child: Icon(Icons.add_circle),
+                      ),
+                    ],
                   ),
-                  SizedBox(width: 8.0),
-                  Text('$people'),
-                  SizedBox(width: 8.0),
-                  GestureDetector(
-                    onTap: () {
-                      peopleIncrement();
-                    },
-                    child: Icon(Icons.add_circle),
+                  SizedBox(height: 20.0),
+                  Row(
+                    children: [
+                      Text('People'),
+                      Spacer(),
+                      GestureDetector(
+                        onTap: () {
+                          peopleDecrement();
+                        },
+                        child: Icon(Icons.remove_circle),
+                      ),
+                      SizedBox(width: 8.0),
+                      Text('$people'),
+                      SizedBox(width: 8.0),
+                      GestureDetector(
+                        onTap: () {
+                          peopleIncrement();
+                        },
+                        child: Icon(Icons.add_circle),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              SizedBox(height: 40.0),
-              FlatButton(
-                onPressed: () {
-                  if (billAmount > 0) {
-                    calculate();
-                  }
+                  SizedBox(height: 40.0),
+                  FlatButton(
+                    onPressed: () {
+                      if (billAmount > 0) {
+                        calculate();
+                      }
 
-                  FocusScope.of(context).unfocus();
-                },
-                padding: EdgeInsets.symmetric(horizontal: 36.0, vertical: 12.0),
-                color: Colors.blue,
-                textColor: Colors.white,
-                child: Text(
-                  'Calculate',
-                  style:
-                      TextStyle(fontSize: 17.0, fontWeight: FontWeight.normal),
-                ),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25.0)),
+                      FocusScope.of(context).unfocus();
+                    },
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 36.0, vertical: 12.0),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    child: Text(
+                      'Calculate',
+                      style: TextStyle(
+                          fontSize: 17.0, fontWeight: FontWeight.normal),
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0)),
+                  ),
+                  SizedBox(height: 20.0),
+                  tipAmount != 0
+                      ? Column(
+                          children: [
+                            Text(
+                              'Total Amount : \$$totalAmount',
+                              style: TextStyle(
+                                color: Colors.black45,
+                              ),
+                            ),
+                            SizedBox(height: 5.0),
+                            Text(
+                              'Tip : \$${(tipAmount / people).toDouble()} per person!',
+                              style: TextStyle(
+                                color: Colors.black45,
+                              ),
+                            ),
+                          ],
+                        )
+                      : Container(),
+                ],
               ),
-              SizedBox(height: 20.0),
-              tipAmount != 0
-                  ? Column(
-                      children: [
-                        Text(
-                          'Total Amount : \$$totalAmount',
-                          style: TextStyle(
-                            color: Colors.black45,
-                          ),
-                        ),
-                        SizedBox(height: 5.0),
-                        Text(
-                          'Tip : \$${(tipAmount / people).toDouble()} per person!',
-                          style: TextStyle(
-                            color: Colors.black45,
-                          ),
-                        ),
-                      ],
-                    )
-                  : Container(),
-            ],
+            ),
           ),
         ),
       ),
